d8213cce5bf3e207357fd722317eefb1df620f6e
tmastrom <thomas.mastromonaco@gmail.com>
2025-07-28 23:50:42 -0700

    go example server and docker container

---
+++ go-example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Go in Docker!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
+		for name, headers := range r.Header {
+			for _, h := range headers {
+				fmt.Fprintf(w, "%v: %v\n", name, h)
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	fmt.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- http-server-go/go.mod
+++
@@ -1,3 +0,0 @@
-module example.com/my_module
-
-go 1.24.4
